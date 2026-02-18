1018a24ad560e0a4860c0ab415375d8dbefc989d
Artur Venzel <venzelartur@mail.ru>
2026-02-18 15:27:57 +0700

    Update setup.py
    K typo mismatch is fixed

--- setup.py
+++ setup.py
@@ -1,55 +1,26 @@
-#!/usr/bin/env python
-# -*- Mode: python; tab-width: 4; indent-tabs-mode:nil; coding:utf-8 -*-
-
-#    setup.py
-#
-#    AFToolKit setup.
-#
-
-
-# Standard library
-from setuptools import setup, find_packages
-
 # Setup options
-name = "AFToolKit"
-
+name = "AFToolKit"  # This is the name on PyPI/pip list
 author = "Shashkova Tatiana, Sindeeva Maria, Ivanisenko Nikita, Telepov Alexander"
-
 version = "1.0.0"
+description = "Python library for routine protein engineering tasks using AlphaFold2."
 
-description = \
-    "Python library for routine protein engineering tasks using AlphaFold2."
-
+# Ensure these keys match the folder name exactly
 package_dir = {"AFToolKit": "AFToolKit"}
 
+# find_packages will now look for the AFToolKit folder
 packages = find_packages(exclude=['tests'])
 
-package_data = \
-    {"AFToolKit": ["processing/*",
-                   "processing/openfold/*",
-                   "models/*"
-                   ]}
+package_data = {
+    "AFToolKit": [
+        "processing/*",
+        "processing/openfold/*",
+        "models/*"
+    ]
+}
 
-entry_points = \
-    {"console_scripts": \
-         ["run_protein_task=AFToolKit.processing.run_protein_task:main",
-          "run_protein_complex_task=AFToolKit.processing.run_protein_complex_task:main"],
-     }
-
-install_requires = ["biopython",
-                    "numpy",
-                    "pandas",
-                    "Biopandas"]
-
-# Run the setup
-setup(name=name,
-      # url=url,
-      author=author,
-      version=version,
-      description=description,
-      include_package_data=True,
-      package_data=package_data,
-      package_dir=package_dir,
-      packages=packages,
-      entry_points=entry_points,
-      install_requires=install_requires)
+entry_points = {
+    "console_scripts": [
+        "run_protein_task=AFToolKit.processing.run_protein_task:main",
+        "run_protein_complex_task=AFToolKit.processing.run_protein_complex_task:main"
+    ],
+}
